44193cdaed0d5cfadfc477a802c1d626b2f29754
vincent.corbel <vincent.corbel@accenture.com>
2017-12-08 14:46:48 +0100

    Add on delete cascade on signalement_dashboard_user_fk constraint

--- src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.1.sql
+++ src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.1.sql
@@ -9,3 +9,9 @@ DELETE FROM workflow_workflow WHERE id_workflow = 1;
 
 -- DMR 146 Suppression de droits obsolètes
 DELETE FROM profile_right WHERE id_right NOT IN (SELECT id_right FROM core_admin_right);
+
+-- DMR 146 Supression d'un utilisateur
+ALTER TABLE signalement_dashboard_user_preferences 
+   DROP CONSTRAINT signalement_dashboard_user_fk,
+   ADD CONSTRAINT signalement_dashboard_user_fk FOREIGN KEY (fk_id_user)
+        REFERENCES core_admin_user (id_user) ON DELETE CASCADE;
