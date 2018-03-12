119aaa034c34a62c27ce1a3661caa3a2549b198b
Julien NICOLAS <julien.nicolas@accenture.com>
2018-03-12 15:11:10 +0100

    DMR-831

--- src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.7.sql
+++ src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.7.sql
@@ -3,3 +3,13 @@ update signalement_signaleur
 set mail = '',
 	guid = ''
 where id_signaleur in (select id_signaleur from signalement_signaleur where mail = 'cora@paris.fr');
+
+/* DMR-831 */
+delete
+from
+	profile_right
+where
+	profile_key in(
+		'DPE_CHEFDESECTEUR',
+		'DPE_FEUILLEDEROUTE'
+	);
