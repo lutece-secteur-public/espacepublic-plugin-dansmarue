b0d21a1d1af1f27097ea593fd091b0a525d13a14
Julien NICOLAS <julien.nicolas@accenture.com>
2018-03-13 11:42:20 +0100

    DMR-154

--- src/sql/plugins/signalement/upgrade/update_adresses_6.0.7.sql
+++ src/sql/plugins/signalement/upgrade/update_adresses_6.0.7.sql
@@ -91,3 +91,81 @@ where id_adresse = 82479;
 update signalement_adresse
 set adresse = '11 rue Audubon, 75012 PARIS'
 where id_adresse = 47630;
+
+/* DMR-154 */
+
+update signalement_adresse
+set adresse = '205 Avenue Daumesnil, 75012 PARIS'
+where id_adresse = 278227;
+
+update signalement_adresse
+set adresse = '9 Rue Berzélius, 75017 PARIS'
+where id_adresse = 275282;
+
+update signalement_adresse
+set adresse = '123 Boulevard de Ménilmontant, 75011 PARIS'
+where id_adresse = 276142;
+
+update signalement_adresse
+set adresse = '33 Rue Cuvier, 75005 PARIS'
+where id_adresse = 276801; 
+
+update signalement_adresse
+set adresse = 'Rue Charles Monselet, 75019 PARIS'
+where id_adresse = 280030;
+
+update signalement_adresse
+set adresse = '33 Avenue Reille, 75014 PARIS'
+where id_adresse = 280455; 
+
+update signalement_adresse
+set adresse = '192 Rue de Crimée, 75019 PARIS'
+where id_adresse = 281355;
+
+update signalement_adresse
+set adresse = '11 Rue Pierre Fontaine, 75009 PARIS'
+where id_adresse = 283464;
+
+update signalement_adresse
+set adresse = '18 Rue du Temple, 75004 PARIS'
+where id_adresse = 285221;
+
+update signalement_adresse
+set adresse = '10 Cours de Vincennes, 75012 PARIS'
+where id_adresse = 285820;
+
+update signalement_adresse
+set adresse = 'Rue Robert Esnault Pelterie, 75007 PARIS'
+where id_adresse = 285994;
+
+update signalement_adresse
+set adresse = '17 Impasse Martini, 75010 PARIS'
+where id_adresse = 287310;
+
+update signalement_adresse
+set adresse = '13 Boulevard Jules Ferry, 75011 PARIS'
+where id_adresse = 288501;
+
+update signalement_adresse
+set adresse = '27 Passage Charles Dallery, 75011 PARIS'
+where id_adresse = 2562;
+
+update signalement_adresse
+set adresse = '1 Rue Pelleport, 75020 PARIS'
+where id_adresse = 289870;
+
+update signalement_adresse
+set adresse = '110 Rue Ordener, 75018 PARIS'
+where id_adresse = 291217;
+
+update signalement_adresse
+set adresse = '10 Rue Gassendi, 75014 PARIS'
+where id_adresse = 293073;
+
+update signalement_adresse
+set adresse = '73 Rue d''Alésia, 75014 PARIS'
+where id_adresse = 287441;
+
+update signalement_adresse
+set geom = ST_SetSRID(ST_MakePoint(2.3151021999999557, 48.8620177), 4326)
+where id_adresse = 285994;
