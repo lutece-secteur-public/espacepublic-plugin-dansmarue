8fc25b794d0de6d48506e44f4ac9a79c9e03ddcc
Julien NICOLAS <julien.nicolas@accenture.com>
2018-05-24 12:01:46 +0200

    Rajout du secteur Ramen à la Racine

--- src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.12.sql
+++ src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.12.sql
@@ -4,3 +4,4 @@ set name = 'Ramen'
 where id_sector = 50013;
 
 insert into unittree_unit_sector(id_unit,id_sector) values(1521,50013);
+insert into unittree_unit_sector(id_unit,id_sector) values(0,50013);
