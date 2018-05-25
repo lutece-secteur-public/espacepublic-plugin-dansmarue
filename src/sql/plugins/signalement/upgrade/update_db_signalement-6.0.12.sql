b63a272497e93ecdf97e2c14d1da1c53d8d25ba3
Julien NICOLAS <julien.nicolas@accenture.com>
2018-05-25 09:55:36 +0200

    Mise à jour des script pour Ramen Prestataire

--- src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.12.sql
+++
@@ -1,7 +0,0 @@
--- Ajout des secteurs au prestataire Ramen créé précédemment
-update unittree_sector
-set name = 'Ramen'
-where id_sector = 50013;
-
-insert into unittree_unit_sector(id_unit,id_sector) values(1521,50013);
-insert into unittree_unit_sector(id_unit,id_sector) values(0,50013);
---
+++ src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.13.sql
@@ -0,0 +1,13 @@
+-- Ajout des secteurs au prestataire Ramen créé précédemment
+update unittree_sector
+set name = 'Ramen'
+where id_sector = 50013;
+
+insert into unittree_unit_sector(id_unit,id_sector) values( (select id_unit from unittree_unit where label = 'Ramen') ,50013);
+insert into unittree_unit_sector(id_unit,id_sector) values(0,50013);
+
+update signalement_type_signalement
+set fk_id_unit = (select id_unit from unittree_unit where label = 'Ramen')
+where fk_id_type_signalement = 1000
+and id_type_signalement not in ( 1006,1007,1008, 11013 );
+
