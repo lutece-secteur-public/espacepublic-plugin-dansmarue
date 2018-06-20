9b2aab1b661169099429b0de86df38d2cf375113
Julien NICOLAS <julien.nicolas@accenture.com>
2018-06-20 16:30:41 +0200

    DMR-905

--- src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.13.sql
+++ src/sql/plugins/signalement/upgrade/update_db_signalement-6.0.13.sql
@@ -45,4 +45,10 @@ where
 			id_state = 11
 	);
 	
+-- Rajout du delete en cascade sur la table observation_rejet
+ alter table
+	public.signalement_observation_rejet_signalement 
+	drop constraint observation_rejet_signalement_fk,
+	add constraint observation_rejet_signalement_fk foreign key (fk_id_signalement) references signalement_signalement(id_signalement) on delete cascade;
+	
 
